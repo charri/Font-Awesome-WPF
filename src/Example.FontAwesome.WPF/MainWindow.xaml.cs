0dc20b7cdb9602897e004b5568600ad6ec9f447d
charri <thomascharriere@gmail.com>
2014-11-22 12:10:27 +0100

    mod: example project

--- src/Example.FontAwesome.WPF/MainWindow.xaml.cs
+++ src/Example.FontAwesome.WPF/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Example.Font_Awesome_WPF.ViewModel;
+using Example.FontAwesome.WPF.ViewModel;
 
-namespace Example.Font_Awesome_WPF
+namespace Example.FontAwesome.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
