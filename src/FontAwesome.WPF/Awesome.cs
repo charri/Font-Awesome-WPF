89cea9e8986f8ce0cd05a7a72fa5efdb089b8607
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2015-07-04 16:09:16 +0200

    Added Awesome.Content attached dependency property

---
+++ src/FontAwesome.WPF/Awesome.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FontAwesome.WPF
+{
+    public static class Awesome
+    {
+        /// <summary>
+        /// Identifies the FontAwesome.WPF.Awesome.Content attached dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.RegisterAttached(
+                "Content",
+                typeof(FontAwesomeIcon),
+                typeof(Awesome),
+                new PropertyMetadata(_defaultContent, ContentChanged));
+
+        /// <summary>
+        /// Gets the content of a ContentControl, expressed as a FontAwesome icon.
+        /// </summary>
+        /// <param name="target">The ContentControl subject of the query</param>
+        /// <returns>FontAwesome icon found as content</returns>
+        public static FontAwesomeIcon GetContent(DependencyObject target)
+        {
+            return (FontAwesomeIcon)target.GetValue(ContentProperty);
+        }
+        
+        /// <summary>
+        /// Sets the content of a ContentControl expressed as a FontAwesome icon. This will cause the content to be redrawn.
+        /// </summary>
+        /// <param name="target">The ContentControl where to set the content</param>
+        /// <param name="value">FontAwesome icon to set as content</param>
+        public static void SetContent(DependencyObject target, FontAwesomeIcon value)
+        {
+            target.SetValue(ContentProperty, value);
+        }
+
+        private static void ContentChanged(DependencyObject sender, DependencyPropertyChangedEventArgs evt)
+        {
+            if (sender is ContentControl)
+            {
+                ContentControl target = sender as ContentControl;
+
+                if (evt.NewValue is FontAwesomeIcon)
+                {
+                    FontAwesomeIcon symbolIcon = (FontAwesomeIcon)evt.NewValue;
+                    int symbolCode = (int)symbolIcon;
+                    char symbolChar = (char)symbolCode;
+
+                    target.Content = symbolChar;
+                }
+                else
+                {
+                    // Just ignore, Awesome.Content property can only be set to a FontAwesomeIcon value
+                }
+            }
+            else
+            {
+                // Just ignore, Awesome.Content property can only be set on a ContentControl element
+            }
+        }
+
+        private static readonly FontAwesomeIcon _defaultContent = FontAwesomeIcon.None;
+    }
+}
