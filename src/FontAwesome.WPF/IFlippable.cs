eb18c552dcd531d7c1ba3f8d30e7c6ccd902aebb
Jan Karger <punkerat76@gmail.com>
2015-10-15 15:04:24 +0200

    add IFlippable and IRotatable

---
+++ src/FontAwesome.WPF/IFlippable.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FontAwesome.WPF
+{
+    /// <summary>
+    /// Defines the different flip orientations that a icon can have.
+    /// </summary>
+    [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+    public enum FlipOrientation
+    {
+        Normal,
+        Horizontal,
+        Vertical,
+    }
+
+    /// <summary>
+    /// Represents a flippable control
+    /// </summary>
+    public interface IFlippable
+    {
+        /// <summary>
+        /// Gets or sets the current orientation (horizontal, vertical) of the icon.
+        /// </summary>
+        FlipOrientation Orientation { get; set; }
+    }
+}
---
+++ src/FontAwesome.WPF/IRotatable.cs
@@ -0,0 +1,13 @@
+﻿namespace FontAwesome.WPF
+{
+    /// <summary>
+    /// Represents a rotatable control
+    /// </summary>
+    public interface IRotatable
+    {
+        /// <summary>
+        /// Gets or sets the current rotation (angle) of the icon.
+        /// </summary>
+        double Rotation { get; set; }
+    }
+}
