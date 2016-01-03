edb94c9d7f785491f7b396db466d55464addb644
charri <git@a.charri.ch>
2016-01-03 15:54:38 +0100

    add converter namespace to XmlnsDefinition

--- src/FontAwesome.WPF/Properties/AssemblyInfo.cs
+++ src/FontAwesome.WPF/Properties/AssemblyInfo.cs
@@ -28,3 +28,4 @@ using System.Windows.Markup;
 
 [assembly: XmlnsPrefix("http://schemas.fontawesome.io/icons/", "fa")]
 [assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF.Converters")]
