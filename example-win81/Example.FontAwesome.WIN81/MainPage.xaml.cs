bcb3eafd597fd31b42cd1e6e5f93821234d20e6f
Miha Markič <miha@rthand.com>
2016-05-10 11:05:30 +0200

    added win81 library and example

---
+++ example-win81/Example.FontAwesome.WIN81/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Example.FontAwesome.WIN81.ViewModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Example.FontAwesome.WIN81
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public MainViewModel ViewModel => (MainViewModel)DataContext;
+
+        private void FilterText_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            ViewModel.Filter(FilterText.Text);
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0)
+                return;
+
+            ViewModel.CurrentIcon = (IconDescription)e.AddedItems[0];
+        }
+    }
+}
