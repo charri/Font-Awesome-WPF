b52a8a6a02642108b0c054e3eaa26cce5f456425
Volodymyr Baydalka <bvsevolod@gmail.com>
2016-06-02 16:02:02 +0300

    Added: markup extension

---
+++ src/WPF/FontAwesome.WPF/FontAwesomeExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+
+namespace FontAwesome.WPF
+{
+    /// <summary>
+    /// Markup extension for easy converting from icon enum to string
+    /// </summary>
+    public class FontAwesomeExtension : MarkupExtension
+    {
+        /// <summary>
+        /// Icon.
+        /// </summary>
+        public FontAwesomeIcon Icon { get; set; }
+
+        public FontAwesomeExtension()
+        {
+        }
+
+
+        public FontAwesomeExtension(FontAwesomeIcon icon)
+        {
+            this.Icon = icon;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return char.ConvertFromUtf32((int)Icon);
+        }
+    }
+}
